af2cb77a136fabedaf8217a9c6900bb25576d2a1
Tim Niven <tim.niven.public@gmail.com>
2022-02-23 09:51:43 +0800

    bump data-structures version

--- tests/test_functions/test_tokens/test_either_slash.py
+++ tests/test_functions/test_tokens/test_either_slash.py
@@ -1,5 +1,7 @@
 import unittest
 
+from data_structures.nlp import Token
+
 from text_cleaning.functions.tokens import ReplaceEitherSlashWithSpace
 
 
@@ -10,4 +12,19 @@ class TestReplaceEitherSlashWithSpace(unittest.TestCase):
         tokens = ['and/or', 'something']
         result = fn(tokens)
         expected = ['and', 'or', 'something']
+        self.assertEqual(expected, result)
+
+    def test_double_spaces_also_removed(self):
+        # NOTE: this test case isn't testing what it's supposed to
+        # TODO: figure out where the problem is actually coming from
+        fn = ReplaceEitherSlashWithSpace()
+        tokens = [
+            Token('this and/or', is_entity=True),
+            Token('something'),
+        ]
+        result = fn(tokens)
+        expected = [
+            'this and or',
+            'something',
+        ]
         self.assertEqual(expected, result)
