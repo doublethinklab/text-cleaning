3c786dac08359d3255a7879504813f01bdf033e4
Tim Niven <tim.niven.public@gmail.com>
2022-03-06 11:07:58 +0800

    default replacement for replacequotes

--- text_cleaning/functions/text/quotes.py
+++ text_cleaning/functions/text/quotes.py
@@ -1,8 +1,20 @@
 from quote_extraction.direct_quotes import replace
+from text_cleaning import replacements
 from text_cleaning.functions.base import ReplaceInText
 
 
 class ReplaceQuotes(ReplaceInText):
+
+    def __init__(
+            self,
+            replacement: str = replacements.QUOTE,
+            logger=None,
+            debug: bool = False
+    ):
+        super().__init__(
+            replacement=replacement,
+            logger=logger,
+            debug=debug)
 
     def clean(self, text: str, **kwargs) -> str:
         text = replace(text, replacement=self.replacement)
